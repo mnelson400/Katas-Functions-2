f08bce6e046bbcd06b261f724c77542b289c291c
Mark Nelson <mnelson400@comcast.net>
2020-05-28 22:19:03 -0500

    Commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// Kata 01
+function add (a, b) {
+    return a + b;
+}
+console.log('Kata 01', add (2, 4))
+
+
+// Kata 02
+function multiply (x, y) {
+    for (let position = 0; position < y; position += 1) {
+        const variableOne = add (x, x);  
+    }
+}
+console.log ('Kata 02', multiply (6, 4))
+
+// Kata 03
+function power () {
+    for (let position = 0; position < variable; position += 1) {       
+    }
+}
+console.log ('Kata 03', power (2, 8))
+
+// Kata 04
+function factorial () {
+    for (let position = 0; position < variable; position += 1) {  
+    }
+}
+console.log ('Kata 04', factorial (x, y))
+
+// Bonus: Fibonacci
+function fibonacci () {
+    for (let position = 0; position < variable; position += 1) {  
+    }
+}
+console.log ('Fibonacci', fibonacci (x, y))
+
