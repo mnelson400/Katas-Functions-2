cd6db63883ece4bc8ebe9a8e8e3ffbb81a67f396
Mark Nelson <mnelson400@comcast.net>
2020-05-31 19:56:34 -0500

    Finished Rubric

--- main.js
+++ main.js
@@ -1,36 +1,81 @@
 // Kata 01
-function add (a, b) {
-    return a + b;
+function add (x, y) {
+
+    return x + y;
 }
-console.log('Kata 01', add (2, 4))
+console.log('Kata 01:', add (2, 4));
 
 
 // Kata 02
 function multiply (x, y) {
-    for (let position = 0; position < y; position += 1) {
-        const variableOne = add (x, x);  
-    }
+
+    let Total = 0;
+
+    let multiplyArray = [];    
+    
+    for (let position = 1; position <= Number(y); position += 1) {
+
+        let newTotal = add (Number(x), Number(Total));
+
+        Total = newTotal;
+
+        multiplyArray.push(newTotal);
+    } 
+
+    return Total;
 }
-console.log ('Kata 02', multiply (6, 4))
+console.log ('Kata 02:', multiply (6, 4));
+
 
 // Kata 03
-function power () {
-    for (let position = 0; position < variable; position += 1) {       
+function power (x, y) {
+
+    let Total = x;
+
+    let multiplyArray = [];    
+    
+    for (let position = 1; position <= Number(y) - 1; position += 1) {
+
+        let newTotal = multiply (Number(x), Number(Total));
+
+        Total = newTotal;
+
+        multiplyArray.push(newTotal);
     }
+
+    return Total;
 }
-console.log ('Kata 03', power (2, 8))
+console.log ('Kata 03:', power (2, 8));
+
 
 // Kata 04
-function factorial () {
-    for (let position = 0; position < variable; position += 1) {  
+function factorial (x) {
+
+    let Total = 1;   
+    
+    for (let position = 2; position <= Number(x); position += 1) {
+
+        let newTotal = multiply (Number(Total), Number(position));
+
+        Total = newTotal;
     }
+
+    return Total;
 }
-console.log ('Kata 04', factorial (x, y))
+console.log ('Kata 04:', factorial (4));
+
+
 
 // Bonus: Fibonacci
-function fibonacci () {
-    for (let position = 0; position < variable; position += 1) {  
-    }
-}
-console.log ('Fibonacci', fibonacci (x, y))
+function fibonacci (x) { 
 
+    let fiboArray = [0, 1];
+
+    for (let position = fiboArray.length; position < Number(x); position += 1) {
+
+    fiboArray[position] = add (fiboArray[position-2], fiboArray[position-1]);
+    // console.log(fiboArray);
+    }
+    return fiboArray
+}
+console.log ('Fibonacci Kata:', fibonacci (8));
